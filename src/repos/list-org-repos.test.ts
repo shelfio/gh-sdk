27696abbada9fbd9453174333165e4b16afeb0fe
Vlad Holubiev <golubev.vld@gmail.com>
2020-01-29 18:34:36 +0200

    test: cover listOrgRepos w/ test

---
+++ src/repos/list-org-repos.test.ts
@@ -0,0 +1,33 @@
+jest.mock('../rest-client');
+
+import {getClient} from '../rest-client';
+import {listOrgRepos} from './list-org-repos';
+
+const listForOrgMock = jest.fn().mockResolvedValue({
+  data: [{a: 1}]
+});
+
+(getClient as jest.Mock).mockReturnValue({
+  repos: {
+    listForOrg: listForOrgMock
+  }
+});
+
+it('should call sdk w/ proper params', async () => {
+  await listOrgRepos('shelf');
+
+  expect(listForOrgMock).toHaveBeenCalledWith({
+    direction: 'desc',
+    org: 'shelf',
+    page: 1,
+    per_page: 100,
+    sort: 'pushed',
+    type: 'all'
+  });
+});
+
+it('should return 1 org repo', async () => {
+  const prs = await listOrgRepos('shelf');
+
+  expect(prs).toEqual([{a: 1}]);
+});
