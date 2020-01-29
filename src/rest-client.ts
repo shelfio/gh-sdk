36783f31048c45172f462cf5084448a1740777af
Vlad Holubiev <golubev.vld@gmail.com>
2020-01-29 17:35:22 +0200

    fix: use new auth way of sdk

--- src/rest-client.ts
+++ src/rest-client.ts
@@ -1,19 +1,15 @@
 import Oktokit from '@octokit/rest';
 
 let client;
-const octokit = new Oktokit();
 
 export function getClient(): Oktokit {
   if (client) {
     return client;
   }
 
-  octokit.authenticate({
-    type: 'oauth',
-    token: process.env.GITHUB_TOKEN
+  client = new Oktokit({
+    auth: process.env.GITHUB_TOKEN
   });
-
-  client = octokit;
 
   return client;
 }
