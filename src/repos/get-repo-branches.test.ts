05c906f0e4369b1ebcb2f545db35e30b9d0bda00
terret <andrew.petlya@gmail.com>
2020-03-26 20:03:12 +0200

    test: add tests for repo branches

---
+++ src/repos/get-repo-branches.test.ts
@@ -0,0 +1,50 @@
+jest.mock('../rest-client');
+
+import {getRepoBranch, getRepoBranches, getRepoBranchesNames} from './get-repo-branches';
+import {getClient} from '../rest-client';
+
+const branchOne = {
+  _links: {html: 'some-html'},
+  name: 'some-branch-name-one'
+};
+
+const branchOTwo = {
+  _links: {html: 'some-html'},
+  name: 'some-branch-name-two'
+};
+
+const branchMock = jest.fn().mockResolvedValue({
+  data: branchOne
+});
+
+const branchesMock = jest.fn().mockResolvedValue({
+  data: [branchOne, branchOTwo]
+});
+
+(getClient as jest.Mock).mockReturnValue({
+  repos: {
+    getBranch: branchMock,
+    listBranches: branchesMock
+  }
+});
+
+const owner = 'some-onwer-name';
+const repo = 'some-repo-name';
+
+it('getRepoBranch should return correct response', async () => {
+  const response = await getRepoBranch({owner, repo, branch: branchOne.name});
+
+  expect(response).toEqual(branchOne);
+});
+
+it('getRepoBranches should return correct response', async () => {
+  const response = await getRepoBranches({owner, repo});
+
+  expect(response).toEqual([branchOne, branchOTwo]);
+});
+
+it('getRepoBranchesNames should return correct response', async () => {
+  const response = await getRepoBranchesNames({owner, repo});
+
+  expect(response).toEqual(['some-branch-name-one', 'some-branch-name-two']);
+});
