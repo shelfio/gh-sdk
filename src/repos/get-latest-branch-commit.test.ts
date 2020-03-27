a722dacf0053364f3c799a102e97641b2a93634d
terret <andrew.petlya@gmail.com>
2020-03-27 19:55:04 +0200

    test: add tests for getting latest commit/ commit.sha

---
+++ src/repos/get-latest-branch-commit.test.ts
@@ -0,0 +1,66 @@
+jest.mock('../rest-client');
+
+import {
+  getLatestBranchCommit,
+  getLatestDevelopCommit,
+  getLatestDevelopCommitSHA
+} from './get-latest-branch-commit';
+import {getClient} from '../rest-client';
+
+const commit = {
+  author: {id: 123, login: 'some-login-string', url: 'some-author-url'},
+  committer: {id: 123, login: 'some-login-string', url: 'some-commiter-string'},
+  parents: [
+    {
+      sha: 'some-parent-hash',
+      url: 'some-parent-url'
+    }
+  ],
+  node_id: 'some-node-id',
+  sha: 'some-commit-hash',
+  url: 'some-commit-url'
+};
+
+const branchResponse = {
+  commit,
+  name: 'some-branch-name',
+  protected: false,
+  protection: {enabled: false},
+  protection_url: 'some-protection-url'
+};
+
+const branchMock = jest.fn().mockResolvedValue({
+  data: branchResponse
+});
+
+(getClient as jest.Mock).mockReturnValue({
+  repos: {
+    getBranch: branchMock
+  }
+});
+
+beforeAll(() => {
+  getClient();
+});
+
+const owner = 'some-owner-name';
+const repo = 'some-repo-name';
+const branch = 'some-branch-name';
+
+it('getLastBranchCommit should return correct result', async () => {
+  const result = await getLatestBranchCommit({owner, repo, branch});
+
+  expect(result).toEqual(commit);
+});
+
+it('getLastDevelopCommit should return correct result', async () => {
+  const result = await getLatestDevelopCommit({owner, repo});
+
+  expect(result).toEqual(commit);
+});
+
+it('getLastDevelopCommitSHA should return correct result', async () => {
+  const result = await getLatestDevelopCommitSHA({owner, repo});
+
+  expect(result).toEqual(commit.sha);
+});
