8d84708b6ed49e46a135e6c7110673dcfe499566
Vlad Holubiev <golubev.vld@gmail.com>
2021-06-21 18:13:15 +0300

    fix: export approve + merge commands

--- src/prs/index.ts
+++ src/prs/index.ts
@@ -1,4 +1,6 @@
+export * from './approve';
 export * from './create-release-pr';
+export * from './extract-repo-name';
 export * from './get-pr';
 export * from './list-prs';
-export * from './extract-repo-name';
+export * from './merge';
