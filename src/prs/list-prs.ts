4ed316c8cf16f046ae95f0fc7c9aac20fd8a2831
Andrii_Petlovanyi <andrew.petlya@gmail.com>
2022-04-16 17:12:17 +0200

    feat: swithed getting list PRs to generator instead of external packages

--- src/prs/list-prs.ts
+++ src/prs/list-prs.ts
@@ -1,55 +1,65 @@
-import {RestEndpointMethodTypes} from '@octokit/plugin-rest-endpoint-methods';
-import {all, paginate} from 'paginate-generator';
+import type {RestEndpointMethodTypes} from '@octokit/plugin-rest-endpoint-methods';
 import {getClient} from '../rest-client';
 
-interface ListOpenPRsParams {
+type ListOpenPRsParams = {
   owner: string;
   searchText?: string;
-}
+};
 
-interface ListPRsParams {
+type ListPRsParams = {
   owner: string;
   searchText?: string;
   prStatus: 'open' | 'closed';
+};
+
+export async function listPrs(params: ListOpenPRsParams): ReturnType<typeof getPRs> {
+  return getPRs({...params, prStatus: 'open'});
 }
 
-export async function listPrs(params: ListOpenPRsParams): ReturnType<typeof listPRs> {
-  return listPRs({...params, prStatus: 'open'});
+export async function listClosedPRs(params: ListOpenPRsParams): ReturnType<typeof getPRs> {
+  return getPRs({...params, prStatus: 'closed'});
 }
 
-export async function listClosedPRs(params: ListOpenPRsParams): ReturnType<typeof listPRs> {
-  return listPRs({...params, prStatus: 'closed'});
-}
+const prsGenerator = async function* (
+  params: ListPRsParams
+): AsyncGenerator<
+  RestEndpointMethodTypes['search']['issuesAndPullRequests']['response']['data']['items']
+> {
+  let pageNumber = 1;
+  let hasNextPage = true;
 
-async function listPRs(
+  while (hasNextPage && pageNumber <= 10) {
+    const gh = getClient();
+
+    const {data} = await gh.search.issuesAndPullRequests({
+      per_page: 100,
+      page: pageNumber,
+      q: `is:${params.prStatus} is:pr archived:false user:${params.owner} ${
+        params.searchText || ''
+      }`,
+    });
+    console.log(`pagenumber: ${pageNumber} || items: ${data.items.length}`);
+
+    if (data.items.length < 100) {
+      hasNextPage = false;
+    }
+
+    pageNumber++;
+
+    yield data.items;
+  }
+};
+
+async function getPRs(
   params: ListPRsParams
 ): Promise<
   RestEndpointMethodTypes['search']['issuesAndPullRequests']['response']['data']['items']
 > {
-  const prs: unknown[] = await all(
-    paginate(async (token?: number) => {
-      if ((token || 1) >= 11) {
-        return {
-          page: [],
-        };
-      }
+  const prs = [];
 
-      const gh = getClient();
-
-      const {data} = await gh.search.issuesAndPullRequests({
-        per_page: 100,
-        page: token || 1,
-        q: `is:${params.prStatus} is:pr archived:false user:${params.owner} ${
-          params.searchText || ''
-        }`,
-      });
-
-      return {
-        next: data.items.length >= 100 ? (token || 1) + 1 : undefined,
-        page: data.items,
-      };
-    })
-  );
+  for await (const prsOnePage of prsGenerator(params)) {
+    prs.push(...prsOnePage);
+  }
 
   return prs as RestEndpointMethodTypes['search']['issuesAndPullRequests']['response']['data']['items'];
 }
