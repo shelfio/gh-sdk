17f00ded69907473718819914243fc4c589d8acf
Yaroslav Chapelskyi <chapelskyi.slavik@gmail.com>
2024-04-17 12:44:42 +0300

    refactor: recreate the client on each getClient()

--- src/rest-client.ts
+++ src/rest-client.ts
@@ -1,15 +1,7 @@
 import {Octokit} from '@octokit/rest';
 
-let client: Octokit;
-
 export function getClient(): Octokit {
-  if (client) {
-    return client;
-  }
-
-  client = new Octokit({
+  return new Octokit({
     auth: process.env.GITHUB_TOKEN,
   });
-
-  return client;
 }
